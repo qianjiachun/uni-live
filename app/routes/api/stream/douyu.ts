83101dc381269576ead9b65b617978b4c6a73d66
qianjiachun <189964430@qq.com>
2023-08-17 15:16:32 +0800

    fix: 修复斗鱼直播流不能获取原画的bug

--- app/routes/api/stream/douyu.ts
+++ app/routes/api/stream/douyu.ts
@@ -1,11 +1,11 @@
 import type { ActionFunction } from "@remix-run/node";
-import { getRealLive_Douyu } from "~/utils/libs/reallive/douyu/reallive";
+import { QN_DOUYU, getRealLive_Douyu } from "~/utils/libs/reallive/douyu/reallive";
 
 
 export const action: ActionFunction = async ({request}) => {
     const {rid, param, qn, type} = await request.json();
-    let postData = `${param}&ver=219032101&rid=${rid}`;
-    let stream = await getRealLive_Douyu(postData, qn || "原画", type || "flv");
+    let postData = `${param}&ver=219032101&cdn=tct-h5&rid=${rid}&rate=${QN_DOUYU[qn || `原画`]}`;
+    let stream = await getRealLive_Douyu(rid, postData, qn || "原画", type || "flv");
     return {
         stream
     };
--- app/utils/libs/reallive/douyu/reallive.ts
+++ app/utils/libs/reallive/douyu/reallive.ts
@@ -1,22 +1,22 @@
 import axios from "axios";
 import { eval1 } from "~/utils";
 
-const QN_DOUYU = {
+export const QN_DOUYU: any = {
   原画: "0",
-  蓝光: "4000p",
-  超清: "2000p",
-  高清: "1200p",
-  流畅: "550p",
+  蓝光: "8",
+  超清: "4",
+  高清: "3",
+  流畅: "2",
 };
 export function getRealLive_DouyuScript(rid: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    axios.get("https://m.douyu.com/" + rid, {
+    axios.get("https://www.douyu.com/" + rid, {
     })
       .then((ret) => {
         let ub9 = "";
-        let a = ret.data.match(/(function ub9.*)[\s\S](var.*)/i);
+        let a = ret.data.match(/(vdwdae325w_64we[\s\S]*?function ub98484234[\s\S]*?)function/i);
         if (a) {
-          ub9 = a[0];
+          ub9 = a[1];
         }
         resolve(ub9);
       })
@@ -27,50 +27,22 @@ export function getRealLive_DouyuScript(rid: string): Promise<string> {
 }
 
 export function getRealLive_Douyu(
+  rid: string,
   data: string,
   qn: IQnType,
   type: IStreamType
 ): Promise<string> {
   return new Promise((resolve, reject) => {
-    axios.post("https://m.douyu.com/api/room/ratestream", data, {
+    axios.post(`https://www.douyu.com/lapi/live/getH5Play/${rid}`, data, {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
     })
       .then((res) => {
         let ret = res.data;
-        let result = "";
-        if (ret.code == "0") {
-          let url = ret.data.url;
-          if (String(url).indexOf("mix=1") != -1) {
-            result = "PKing";
-          } else {
-            let p = /^[0-9a-zA-Z]*/;
-            let tmpArr = String(ret.data.url).split("/");
-            let tmpResult = tmpArr[tmpArr.length - 1].match(p);
-            if (tmpResult) {
-              result = tmpResult[0];
-            }
-          }
-        } else {
-          result = "0";
-        }
-        let cl = QN_DOUYU[qn];
         let realLive = "";
-        if (result == "0") {
-          realLive = "";
-        } else {
-          realLive = String(ret.data.url).replace("m3u8", "flv");
-          realLive = realLive.replace("http:", "https:");
-          // if (cl === "0") {
-          //   realLive = `https://openflv-huos.douyucdn2.cn/dyliveflv1/${result}.m3u8?uuid=`;
-          // } else {
-          //   realLive = `https://openflv-huos.douyucdn2.cn/dyliveflv1/${result}_${cl}.m3u8?uuid=`;
-          // }
-          // if (type === "flv") {
-          //   // realLive = String(ret.data.url).replace("m3u8", "flv");
-			    //   realLive = String(realLive).replace("m3u8", "flv");
-          // }
+        if (ret.error === 0) {
+            realLive = `${ret.data.rtmp_url}/${ret.data.rtmp_live}`;
         }
         resolve(realLive);
       })
